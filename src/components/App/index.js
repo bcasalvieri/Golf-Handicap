41451e400f24ac4ffe1a9ba8d67d37a639a286d8
Brad Cas <bmcasalvieri@gmail.com>
2019-09-16 23:00:19 -0400

    set up authorization

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,9 +5,9 @@ import Navigation from "../Navigation";
 import SignUpPage from "../SignUp";
 import SignInPage from "../SignIn";
 import PasswordForgetPage from "../PasswordForget";
-// import HomePage from "../Home";
+import HomePage from "../Home";
 import AccountPage from "../Account";
-// import AdminPage from "../Admin";
+import AdminPage from "../Admin";
 import * as ROUTES from "../../constants/routes";
 import { withAuthentication } from "../Session";
 
@@ -22,9 +22,9 @@ const App = () => (
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      {/* <Route path={ROUTES.HOME} component={HomePage} /> */}
+      <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
     </div>
   </Router>
 );
